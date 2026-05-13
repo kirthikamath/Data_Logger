5a5f3a40fba8b5b7f773e5b25ab526fa37779765
kirthikamath <80137284+kirthikamath@users.noreply.github.com>
2026-05-13 19:27:26 +0530

    sensor.c

--- sensor.c
+++ sensor.c
@@ -1,1 +1,10 @@
+#include "sensors.h"
 
+void read_temp_humidity_sensor(float *temp, float *hum)
+{
+  // Read sensor registers using I2C
+
+  //Convert raw sensor data
+
+  //Store converted values
+}
