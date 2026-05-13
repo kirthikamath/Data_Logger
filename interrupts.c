49a610ff381207e634ff8d143c5df4b8b371b87b
kirthikamath <80137284+kirthikamath@users.noreply.github.com>
2026-05-13 20:21:04 +0530

    interrupts.c

--- interrupts.c
+++ interrupts.c
@@ -1,1 +1,11 @@
+#include "interrupt.h"
 
+void calculate_rainfall(void)
+{
+  /***********************************************
+    *Each rain guage tip generates one pulse
+    *Interrupt ensure pulses are not missed
+  ************************************************/
+
+rain_pulse_count++;
+}
