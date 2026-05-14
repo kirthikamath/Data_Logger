4b1c4179180b3359f94637844e38fb498c0843f5
kirthikamath <80137284+kirthikamath@users.noreply.github.com>
2026-05-14 15:51:10 +0530

    Update power_management.h

--- power_management.h
+++ power_management.h
@@ -1,1 +1,24 @@
+/***************************************************************
+* Description : Header file for power management functions
+****************************************************************/
+#ifndef POWER_MANAGEMENT_H
+#define POWER_MANAGEMENT_H
 
+/* ==================== FUNCTION PROTOTYPES ===============*/
+
+/***************************************************************
+* Description : Initializes power management subsystem
+****************************************************************/
+void power_management_init(void);
+
+/***************************************************************
+* Description : Puts MCU into low-power sleep mode
+****************************************************************/
+void enter_sleep_mode(void);
+
+/***************************************************************
+* Description : Wakes MCU from sleep mode
+****************************************************************/
+void wakeup_from_sleep(void);
+
+#endif
