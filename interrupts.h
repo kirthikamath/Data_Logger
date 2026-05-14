d40a325a6b998a5cccf1eecf52a245c9f548608b
kirthikamath <80137284+kirthikamath@users.noreply.github.com>
2026-05-14 11:09:56 +0530

    interrupts.h

--- interrupts.h
+++ interrupts.h
@@ -6,16 +6,16 @@
 #define INTERRUPTS_H
 
 /*========================== INCLUDES ===============================*/
-#include<stdint.h>
+#include <stdint.h>
 
 /*========================== EXTERNAL VARAIBLES ======================*/
-/* Rain pulse countyer updated inside ISR */
+/* Rain pulse counter updated inside ISR */
 extern volatile uint32_t rain_pulse_count;
 
 /*========================== FUNCTION PROTOTYPES ====================*/
 
 /************************************************************************
-* Description : Interrupt Service routine for rain gauge pulse detection
+* Description : Interrupt Service Routine for rain gauge pulse detection
 **************************************************************************/
 
 void rain_gauge_ISR(void);
