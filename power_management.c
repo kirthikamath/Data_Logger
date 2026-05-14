223aaa3a8b7bcf5e1acb21af2183c72a1b502520
kirthikamath <80137284+kirthikamath@users.noreply.github.com>
2026-05-14 15:46:01 +0530

    Update power_management.c

--- power_management.c
+++ power_management.c
@@ -1,1 +1,21 @@
+#include "power_management.h"
 
+void power_management_init(void)
+{
+  // Configure low-power settings
+
+  // Configure wakeup sources
+
+}
+
+void enter_sleep_mode(void)
+{
+  // Disable unused peripherals
+
+  //Enter MCU low_power mode
+}
+
+void wakeup_from_sleep(void)
+{
+  // Wakeup using RTC/timer interrupt
+}
