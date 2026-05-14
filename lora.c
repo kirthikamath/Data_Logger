0719170de7ff8eca70a440bf961e210bb959c1df
kirthikamath <80137284+kirthikamath@users.noreply.github.com>
2026-05-14 15:29:58 +0530

    Update lora.c

--- lora.c
+++ lora.c
@@ -1,1 +1,18 @@
+#include "lora.h"
 
+
+void lora_init(void)
+{
+  // Initializw LoRa module
+
+  // Configure communication parameters
+
+  // Verify module readiness
+}
+
+void lora_send(char * data)
+{
+  // Send packet to LoRa module using SPI
+
+  // Trigger wireless transmission
+}
