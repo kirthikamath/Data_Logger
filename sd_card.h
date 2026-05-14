07fd48ff9aa8a1a2e10527723de37d5cf1eee34d
kirthikamath <80137284+kirthikamath@users.noreply.github.com>
2026-05-14 15:12:05 +0530

    Update sd_card.h

--- sd_card.h
+++ sd_card.h
@@ -1,9 +1,20 @@
+/**********************************************************
+* Description : Header file for SD Card handling functions
+***********************************************************/
+
 #ifndef SD_CARD_H
-#define SD_CARD_J
+#define SD_CARD_H
 
 /*====================== FUNCTION PROTOTYPES =============*/
 
+/**********************************************************
+* Description : Initialize SD card and filesystem
+***********************************************************/
 void sd_card_init(void);
+
+/**********************************************************
+* Description : Stores sensor log data into SD Card
+***********************************************************/
 void sd_write_log(char * data);
 
 #endif
