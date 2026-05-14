fe92bba28c6f7ec15917f9d11edb1831b3f367db
kirthikamath <80137284+kirthikamath@users.noreply.github.com>
2026-05-14 11:27:52 +0530

    sensor.c

--- sensor.c
+++ sensor.c
@@ -8,3 +8,13 @@ void read_temp_humidity_sensor(float *temp, float *hum)
 
   //Store converted values
 }
+
+float calculate_rainfall (uint32_t pulse_count)
+{
+  /********************************************************
+    * Each rain gauge tip corresponds to fixed rainfall.
+    * Example: 1 pulse = 0.25mm rainfall
+  ********************************************************/
+
+  return (pulse_count *0.25);
+}
