4df592d03ffe75701b318830fb01eaadbd55f3d3
kirthikamath <80137284+kirthikamath@users.noreply.github.com>
2026-05-14 15:05:43 +0530

    Update sd_card.c

--- sd_card.c
+++ sd_card.c
@@ -1,1 +1,18 @@
+#include "sd_card.h"
 
+void sd_card_init(void)
+{
+  // Initialize SPI communication
+
+  // Detect Sd Card
+
+  // Mount FAT filesystem
+}
+void sd_write_log(char *data)
+{
+  // Open log file
+
+  // Write sensor file
+
+  // Close file safely
+}
