3c918c9d46756483c99e4470ce7bbd52d9bce349
kirthikamath <80137284+kirthikamath@users.noreply.github.com>
2026-05-14 15:34:18 +0530

    Update lora.h

--- lora.h
+++ lora.h
@@ -1,9 +1,20 @@
+/***********************************************************
+* Description : Header file for LoRa communication functions
+************************************************************/
+
 #ifndef LORA_H
 #define LORA_H
 
 /* ===================== FUNCTION PROTOTYPES ==============*/
 
+/***********************************************************
+* Description : Initializes LoRa communication module
+************************************************************/
 void lora_init(void);
+
+/***********************************************************
+* Description : Sends sensor data packet using LoRa
+************************************************************/
 void lora_send(char * data);
 
 #endif
